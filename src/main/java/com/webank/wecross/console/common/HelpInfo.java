a3a4f11765ef5c3b543726aec0320796f05fbd1e
shareong <740310627@qq.com>
2020-08-14 14:11:46 +0800

    <fix>(HelpInfo): update sample command for bcosDeploy and bcosRegister

--- src/main/java/com/webank/wecross/console/common/HelpInfo.java
+++ src/main/java/com/webank/wecross/console/common/HelpInfo.java
@@ -255,12 +255,12 @@ public class HelpInfo {
         System.out.println("Path -- e.g: [zone.chain.res], specify which the path to be deployed");
         System.out.println("Account -- Choose an account to send transaction");
         System.out.println(
-                "Source file path -- The solidity source code file path, e.g: HelloWorld.sol");
+                "Source file path from conf/ -- The solidity source code file path, e.g: HelloWorld.sol");
         System.out.println("Class name -- The contract to be deploy");
         System.out.println("Version -- The contract version");
         System.out.println("Example:");
         System.out.println(
-                "    bcosDeploy payment.bcos.HelloWorld bcos_user1 conf/contracts/solidity/HelloWorld.sol HelloWorld 1.0");
+                "    bcosDeploy payment.bcos.HelloWorld bcos_user1 contracts/solidity/HelloWorld.sol HelloWorld 1.0");
         ConsoleUtils.singleLine();
     }
 
@@ -272,12 +272,12 @@ public class HelpInfo {
         System.out.println("Path -- e.g: [zone.chain.res], specify which the path to be register");
         System.out.println("Account -- Choose an account to send transaction");
         System.out.println(
-                "Source file path -- The solidity source code/solidity abi file path, e.g: HelloWorld.sol or HelloWorld.abi");
+                "Source file path from conf/ -- The solidity source code/solidity abi file path, e.g: HelloWorld.sol or HelloWorld.abi");
         System.out.println("Contract address -- contract address");
         System.out.println("Version -- The contract version");
         System.out.println("Example:");
         System.out.println(
-                "    bcosRegister payment.bcos.HelloWorld bcos_user1 conf/contracts/solidity/HelloWorld.sol 0x2c8595f82dc930208314030abc6f5c4ddbc8864f 1.0");
+                "    bcosRegister payment.bcos.HelloWorld bcos_user1 contracts/solidity/HelloWorld.sol 0x2c8595f82dc930208314030abc6f5c4ddbc8864f 1.0");
         System.out.println(
                 "    bcosRegister payment.bcos.HelloWorld bcos_user1 /data/app/HelloWorld.abi 0x2c8595f82dc930208314030abc6f5c4ddbc8864f 1.0");
         ConsoleUtils.singleLine();
