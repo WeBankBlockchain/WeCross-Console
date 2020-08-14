e788ef1a349fec4b1c093b59273b70157bd48f92
Kyon <32325790+kyonRay@users.noreply.github.com>
2020-08-14 11:25:48 +0800

    优化控制台补全 (#82)
    * update help info (#81)
    
    * perf(JlineUtils): optimize console, support relative filePath Completer, used-methods Completer
    
    * style(JlineUtils):simplify codestyle
    
    Co-authored-by: Shareong <740310627@qq.com>

--- src/main/java/com/webank/wecross/console/common/WelcomeInfo.java
+++ src/main/java/com/webank/wecross/console/common/WelcomeInfo.java
@@ -41,7 +41,7 @@ public class WelcomeInfo {
         sb.append("commitTransaction                  Commit a 2pc transaction.\n");
         sb.append("rollbackTransaction                Rollback a 2pc transaction.\n");
         sb.append("getTransactionInfo                 Get info of specified transaction.\n");
-        sb.append("getTransactionIDs                  Get all transaction ids.\n");
+        sb.append("getTransactionIDs                  Get transaction ids of 2pc.\n");
         sb.append("bcosDeploy                         Deploy contract in BCOS chain.\n");
         sb.append("bcosRegister                       Register contract abi in BCOS chain.\n");
         sb.append("fabricInstall                      Install chaincode in fabric chain.\n");
