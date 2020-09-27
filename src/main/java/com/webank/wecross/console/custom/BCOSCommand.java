54bd156f81a97c170c0e848843f209684f69635b
Kyon <32325790+kyonRay@users.noreply.github.com>
2020-09-27 17:12:20 +0800

    fix BCOS Custom Command (#95)
    * <fix>(BCOSCustom): fix BCOS Custom Command
    
    * <fix>(gradle): update and adapt to new sdk.
    
    Co-authored-by: Kyon <kyonGuo@163.com>

--- src/main/java/com/webank/wecross/console/custom/BCOSCommand.java
+++ src/main/java/com/webank/wecross/console/custom/BCOSCommand.java
@@ -84,7 +84,6 @@ public class BCOSCommand {
         String path = params[1];
         RPCUtils.checkPath(path);
         String cnsName = path.split("\\.")[2];
-        String account = ConsoleUtils.getRuntimeUsername();
         String sourcePath = params[2];
         String className = params[3];
         String version = params[4];
@@ -112,8 +111,7 @@ public class BCOSCommand {
             args.add(ConsoleUtils.parseString(params[i]));
         }
 
-        CommandResponse response =
-                weCrossRPC.customCommand("deploy", path, account, args.toArray()).send();
+        CommandResponse response = weCrossRPC.customCommand("deploy", path, args.toArray()).send();
         PrintUtils.printCommandResponse(response);
     }
 
@@ -137,7 +135,6 @@ public class BCOSCommand {
 
         String path = params[1];
         RPCUtils.checkPath(path);
-        String account = ConsoleUtils.getRuntimeUsername();
         String sourcePath = params[2];
         String address = params[3];
         String cnsName = params[4];
@@ -169,7 +166,7 @@ public class BCOSCommand {
                                 version));
 
         CommandResponse response =
-                weCrossRPC.customCommand("register", path, account, args.toArray()).send();
+                weCrossRPC.customCommand("register", path, args.toArray()).send();
         PrintUtils.printCommandResponse(response);
     }
 }
