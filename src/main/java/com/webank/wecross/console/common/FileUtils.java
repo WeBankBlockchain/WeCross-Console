a6e7801b82d5541f04adfacde342456d3caa2eb8
kyonRay <rui.kwok@outlook.com>
2020-10-09 15:51:04 +0800

    <fix>(RPC,shell): fix login & transaction cache bugs and move listResource to be Auth-required.

--- src/main/java/com/webank/wecross/console/common/FileUtils.java
+++ src/main/java/com/webank/wecross/console/common/FileUtils.java
@@ -3,6 +3,7 @@ package com.webank.wecross.console.common;
 import com.moandjiezana.toml.Toml;
 import com.moandjiezana.toml.TomlWriter;
 import com.webank.wecross.console.exception.WeCrossConsoleException;
+import com.webank.wecross.console.routine.TwoPcFace;
 import com.webank.wecrosssdk.exception.ErrorCode;
 import java.io.File;
 import java.io.IOException;
@@ -40,8 +41,8 @@ public class FileUtils {
 
     public static String mergeSource(
             String currentDir, String sourceFile, PathMatchingResourcePatternResolver resolver)
-            throws Exception {
-        StringBuffer sourceBuffer = new StringBuffer();
+            throws IOException {
+        StringBuilder sourceBuffer = new StringBuilder();
 
         String fullPath = currentDir + sourceFile;
         String dir = fullPath.substring(0, fullPath.lastIndexOf(File.separator)) + File.separator;
@@ -51,12 +52,11 @@ public class FileUtils {
         if (!sourceResource.exists()) {
             logger.error("Source file: {} not found!", fullPath);
 
-            throw new Exception("Source file:" + fullPath + " not found");
+            throw new IOException("Source file:" + fullPath + " not found");
         }
 
         Pattern pattern = Pattern.compile("^\\s*import\\s+[\"'](.+)[\"']\\s*;\\s*$");
-        Scanner scanner = new Scanner(sourceResource.getInputStream(), "UTF-8");
-        try {
+        try (Scanner scanner = new Scanner(sourceResource.getInputStream(), "UTF-8")) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 Matcher matcher = pattern.matcher(line);
@@ -68,21 +68,19 @@ public class FileUtils {
                     sourceBuffer.append(System.lineSeparator());
                 }
             }
-        } finally {
-            scanner.close();
         }
 
         return sourceBuffer.toString();
     }
 
-    public static String readSourceFile(String path) throws Exception {
+    public static String readSourceFile(String path) throws IOException {
         PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         org.springframework.core.io.Resource resource = resolver.getResource("file:" + path);
         if (!resource.exists()) {
             resource = resolver.getResource("classpath:" + path);
             if (!resource.exists()) {
                 logger.error("Source file: {} not exists", path);
-                throw new Exception("Source file: " + path + " not exists");
+                throw new IOException("Source file: " + path + " not exists");
             }
         }
 
@@ -93,7 +91,7 @@ public class FileUtils {
         return mergeSource(dir, filename, resolver);
     }
 
-    public static String readFileContent(String fileName) throws Exception {
+    public static String readFileContent(String fileName) throws IOException {
         try {
             Path path;
 
@@ -108,7 +106,7 @@ public class FileUtils {
             return new String(Files.readAllBytes(path));
         } catch (Exception e) {
             logger.error("Read file error: {}", e.getMessage());
-            throw new Exception("Read file error: " + e.getMessage());
+            throw new IOException("Read file error: " + e.getMessage());
         }
     }
 
@@ -139,17 +137,25 @@ public class FileUtils {
         return transactionPath;
     }
 
-    public static void loadTransactionLog(List<Completer> completers) {
+    public static void loadTransactionLog(List<Completer> completers, TwoPcFace twoPcFace) {
         try {
             Toml toml = getToml(TRANSACTION_LOG_TOML);
             String transactionID = getTransactionID(toml);
             List<String> transactionPath = getTransactionPath(toml);
 
+            if (!twoPcFace.isTransactionInfoExist(
+                    transactionID, transactionPath.toArray(new String[0]))) {
+                logger.error(
+                        "loadTransactionLog error: the transaction in toml file had already been committed/rollbacked or even doesn't exist.");
+                return;
+            }
             TransactionInfo transactionInfo = new TransactionInfo(transactionID, transactionPath);
             ConsoleUtils.runtimeTransactionThreadLocal.set(transactionInfo);
             JlineUtils.addTransactionInfoCompleters(completers);
         } catch (WeCrossConsoleException e) {
             logger.warn("Load transactionLog Toml file fail, error: {}.", e.getMessage());
+        } catch (Exception e) {
+            logger.warn("Load transactionLog Toml file fail, error is " + e.getMessage());
         }
     }
 
