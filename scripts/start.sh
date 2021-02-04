17dfd8a8eaad730fadb4466b7c7516a179e1afc8
octopus <912554887@qq.com>
2021-02-04 12:41:43 +0800

    fix ssl handshake failed on jdk 15

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,17 +2,29 @@
 dirpath="$(cd "$(dirname "$0")" && pwd)"
 cd ${dirpath}
 
+SECURIY_FILE='./.wecross.security'
+
+create_jvm_security()
+{
+  if [[ ! -f ${SECURIY_FILE} ]];then
+    echo "jdk.disabled.namedCurves = " > ${SECURIY_FILE}
+    # LOG_INFO "create new file ${SECURIY_FILE}"
+  fi
+}
+
 function run_console() {
     if [ "$(uname)" == "Darwin" ]; then
         # Mac
-        java -cp 'apps/*:lib/*:conf' com.webank.wecross.console.Shell
+        java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1,x25519,secp256r1,secp384r1,secp521r1,x448,ffdhe2048,ffdhe3072,ffdhe4096,ffdhe6144,ffdhe8192" -cp 'apps/*:lib/*:conf' com.webank.wecross.console.Shell
     elif [ "$(uname -s | grep MINGW | wc -l)" != "0" ]; then
         # Windows
-        java -cp 'apps/*;lib/*;conf' com.webank.wecross.console.Shell
+        java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1,x25519,secp256r1,secp384r1,secp521r1,x448,ffdhe2048,ffdhe3072,ffdhe4096,ffdhe6144,ffdhe8192" -cp 'apps/*;lib/*;conf' com.webank.wecross.console.Shell
     else
         # GNU/Linux
-        java -cp 'apps/*:lib/*:conf' -Djava.security.egd=file:/dev/./urandom com.webank.wecross.console.Shell
+        java -Djava.security.properties=${SECURIY_FILE} -Djdk.sunec.disableNative="false" -Djdk.tls.namedGroups="secp256k1,x25519,secp256r1,secp384r1,secp521r1,x448,ffdhe2048,ffdhe3072,ffdhe4096,ffdhe6144,ffdhe8192" -cp 'apps/*:lib/*:conf' -Djava.security.egd=file:/dev/./urandom com.webank.wecross.console.Shell
     fi
 }
 
+create_jvm_security
 run_console
+
